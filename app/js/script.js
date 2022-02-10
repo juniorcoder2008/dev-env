d0457f0414f4b0f223e29e6327a405cc0a8560b3
JuniorCoder <57005033+juniorcoder2008@users.noreply.github.com>
2022-02-10 16:12:59 +0100

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,14 @@
-const $ = require('jquery');
+// Colors
+const main = '#568FF0';
+const darkMain = '#2F3137';
+const blackMain = '#191A1D';
+
+const green = '#27FFA9';
+const red = '#FF0077';
+const yellow = '#FFCC00';
+
+const white = '#eeeeee';
+const black = '#1b1b1b';
+
+const fullWhite = '#fff';
+const fullBlack = '#000';
