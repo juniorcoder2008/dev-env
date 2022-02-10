383a5bf1b832e21236a13f986ac7ab41893672ad
JuniorCoder <thomas08samuel@gmail.com>
2022-02-10 16:36:38 +0100

    general-update

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,13 @@
 // Colors
-const main = '#568FF0';
-const darkMain = '#2F3137';
-const blackMain = '#191A1D';
-
-const green = '#27FFA9';
-const red = '#FF0077';
-const yellow = '#FFCC00';
-
-const white = '#eeeeee';
-const black = '#1b1b1b';
-
-const fullWhite = '#fff';
-const fullBlack = '#000';
+const colors = {
+    main: '#568FF0',
+    darkMain: '#2F3137',
+    blackMain: '#191A1D',
+    green: '#27FFA9',
+    red: '#FF0077',
+    yellow: '#FFCC00',
+    white: '#eeeeee',
+    black: '#1b1b1b',
+    fullWhite: '#fff',
+    fullBlack: '#000',
+};
